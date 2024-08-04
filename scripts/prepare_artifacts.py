35f21c9c2dda6acf42d204f4fbb9554d1ceaf102
Kevin Romero Peces-Barba <hello@kevinrpb.me>
2024-08-04 14:07:21 -0400

    Setup CI builds for macOS

--- scripts/prepare_artifacts.py
+++ scripts/prepare_artifacts.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import errno
 import os
 import shutil
 import sys
@@ -9,13 +10,17 @@ from pathlib import Path
 
 class MatrixRunsOn(Enum):
     WINDOWS_LATEST = "windows-latest"
+    MACOS_14 = "macos-14"
 
 
 root_dirpath = Path(__file__).parent.parent.resolve()
 src_dirpath = root_dirpath / Path("dist/pyinstaller")
 dst_dirpath = root_dirpath / Path("artifacts")
 
-src_filepaths = {MatrixRunsOn.WINDOWS_LATEST: src_dirpath / Path("win_amd64/yt-dlp-guitk.exe")}
+src_filepaths = {
+    MatrixRunsOn.MACOS_14: src_dirpath / Path("macosx_14_0_arm64/yt-dlp-guitk.app"),
+    MatrixRunsOn.WINDOWS_LATEST: src_dirpath / Path("win_amd64/yt-dlp-guitk.exe"),
+}
 
 
 def main():
@@ -35,7 +40,14 @@ dst: {dst_filepath}
     )
 
     os.makedirs(dst_dirpath, exist_ok=True)
-    shutil.copy(src_filepath, dst_filepath)
+
+    try:
+        shutil.copytree(src_filepath, dst_filepath)
+    except OSError as e:
+        if e.errno in (errno.ENOTDIR, errno.EINVAL):
+            shutil.copy(src_filepath, dst_filepath)
+        else:
+            raise e
 
 
 if __name__ == "__main__":
